466eefb57cff5ec6a510795a85dde1d96b82e272
Shandoz-cafe <rafiffahreza11@gmail.com>
2026-02-26 19:38:03 +0700

    Refactor Firebase initialization and exports
    Updated Firebase imports and configuration for improved functionality.

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXXt6pg-H09RfkgUhnAlZKMsZU8-9oeqw",
+  authDomain: "kasir-10280.firebaseapp.com",
+  projectId: "kasir-10280",
+  storageBucket: "kasir-10280.firebasestorage.app",
+  messagingSenderId: "170037480150",
+  appId: "1:170037480150:web:45d084d73d9f9834401777",
+  measurementId: "G-W4XHNJ2PCH"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
