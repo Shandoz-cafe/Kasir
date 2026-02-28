b38f9fc33747dd715f2b0d7309ea3670180e2fe8
Shandoz-cafe <rafiffahreza11@gmail.com>
2026-03-01 03:28:35 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -9,23 +9,18 @@ function login() {
     const userForm = document.getElementById('loginUser').value.trim();
     const passForm = document.getElementById('loginPass').value;
     const users = JSON.parse(localStorage.getItem('users')) || [];
-    
     const validUser = users.find(u => u.username === userForm && u.password === passForm);
+    
     if(validUser) {
         localStorage.setItem('currentUser', validUser.username);
         localStorage.setItem('currentRole', validUser.role || 'kasir');
         window.location.href = 'dashboard.html';
-    } else {
-        alert('Username atau password salah!');
-    }
+    } else { alert('Username atau password salah!'); }
 }
 
 function logout() {
-    localStorage.removeItem('currentUser');
-    localStorage.removeItem('currentRole');
+    localStorage.removeItem('currentUser'); localStorage.removeItem('currentRole');
     window.location.href = 'index.html';
 }
 
-function checkAuth() {
-    if(!localStorage.getItem('currentUser')) window.location.href = 'index.html';
-}
+function checkAuth() { if(!localStorage.getItem('currentUser')) window.location.href = 'index.html'; }
